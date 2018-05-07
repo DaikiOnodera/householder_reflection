be6b21ff94506294e822e203ffa7c296b10a19bd
DaikiOnodera <daikionodera@DaikiOnoderanoMacBook-Air.local>
2018-05-08 00:04:08 +0900

    add files

---
+++ test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding:utf-8
+
+import pprint
+import scipy
+import scipy.linalg
+
+A = scipy.array([[12, -51, 4], [6, 167, -68], [-4, 24, -41]])
+Q, R = scipy.linalg.qr(A)
+
+print("A:")
+pprint.pprint(A)
+
+print("Q:")
+pprint.pprint(Q)
+
+print("R:")
+pprint.pprint(R)
+
+
